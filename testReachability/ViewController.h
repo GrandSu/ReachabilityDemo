053d79cae75b7f2f6f2eb3141105d034125d27e8
Li Yong <btt365com@gmail.com>
2018-08-07 15:14:27 +0800

    Initial Commit

---
+++ testReachability/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testReachability
+//
+//  Created by bet001 on 2018/8/7.
+//  Copyright © 2018年 bet365. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
